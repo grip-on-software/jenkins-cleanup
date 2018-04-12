d95b4ed0b513084d26d4d2514eea3794467ced95
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-04-12 11:05:45 +0200

    Remove dangling Docker images as well

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker ps -a -f status=exited -q | xargs --no-run-if-empty docker rm
 docker images --filter dangling=false | grep "<none>" | awk  '{print $3}' | xargs --no-run-if-empty docker rmi
+docker images -q --filter dangling=true | xargs --no-run-if-empty docker rmi
