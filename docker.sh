65b77fede35c8955e3a2d8c60071bb4b4d4d188d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2022-03-10 12:05:26 +0100

    Make registry variable

--- docker.sh
+++ docker.sh
@@ -6,5 +6,5 @@ else
 	docker ps -a -f status=exited -q | xargs --no-run-if-empty docker rm
 	docker images --filter dangling=false | grep "<none>" | awk  '{print $3}' | xargs --no-run-if-empty docker rmi
 	docker images -q --filter dangling=true | xargs --no-run-if-empty docker rmi
-	docker images --format "{{.Repository}} {{.Tag}} {{.CreatedAt}}" --filter dangling=false | grep -Ev "\s(<none>|latest|[0-9]+\.[0-9]+\.[0-9]+)\s" | grep "^docker-registry" | awk '{print $1, $2, $3}' > images.txt
+	docker images --format "{{.Repository}} {{.Tag}} {{.CreatedAt}}" --filter dangling=false | grep -Ev "\s(<none>|latest|[0-9]+\.[0-9]+\.[0-9]+)\s" | grep "^$DOCKER_REGISTRY" | awk '{print $1, $2, $3}' > images.txt
 fi
